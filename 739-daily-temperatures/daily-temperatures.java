fb69e62fb75386659a0e71b300571bb9928151f3
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-05-18 15:10:59 -0400

    Time: 69 ms (44.93%) | Memory: 63.4 MB (6.43%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,14 +1,16 @@
+import java.util.AbstractMap;
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Map.Entry<Integer, Integer>> stack = new Stack<>();
         int[] result = new int[temperatures.length];
         int i = 0;
-        while(i < temperatures.length){
-            while(stack.isEmpty() == false && temperatures[stack.peek()] < temperatures[i] ){
-                int index = stack.pop();
-                result[index] = i - index;
+        while( i < temperatures.length){
+            while(stack.isEmpty() == false && stack.peek().getKey() < temperatures[i]){
+                int index = stack.pop().getValue();
+                result[index] = i-index;
             }
-            stack.push(i);
+            Map.Entry<Integer, Integer> curr = new AbstractMap.SimpleEntry<>(temperatures[i], i);
+            stack.push(curr);
             i++;
         }
         return result;
