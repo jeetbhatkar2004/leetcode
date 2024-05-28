c20c3a19653d907c90f71b2159f31d6744d55e52
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-05-28 18:59:32 -0400

    Time: 43 ms (84.63%) | Memory: 112.8 MB (70.41%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,85 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class LRUCache {
+    private class Node {
+        int key, val;
+        Node next, prev;
+        Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    private int capacity, size;
+    private Map<Integer, Node> cache;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.cache = new HashMap<>();
+        this.head = new Node(0, 0);  
+        this.tail = new Node(0, 0);  
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void addToHead(Node node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private void removeLRU() {
+        Node lru = tail.prev;
+        removeNode(lru);
+        cache.remove(lru.key);
+    }
+
+    public int get(int key) {
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+        Node node = cache.get(key);
+        moveToHead(node);
+        return node.val;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.val = value;
+            moveToHead(node);
+        } else {
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            size++;
+            if (size > capacity) {
+                removeLRU();
+                size--;
+            }
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key, value);
+ */
