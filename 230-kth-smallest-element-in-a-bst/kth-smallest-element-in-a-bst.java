2458392645afafba62cd0b0b281ff0f9857fe457
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-01 17:48:27 -0400

    Time: 0 ms (100.00%) | Memory: 44.4 MB (40.05%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -16,31 +16,22 @@
 import java.util.Stack;
 
 class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode current = root;
-        
-        while (current != null || !stack.isEmpty()) {
-            // Go to the leftmost node
-            while (current != null) {
-                stack.push(current);
-                current = current.left;
-            }
-            
-            // Current node must be the leftmost
-            current = stack.pop();
-            k--;
-            
-            // If we have reached the k-th smallest, return its value
-            if (k == 0) {
-                return current.val;
-            }
-            
-            // Move to the right subtree
-            current = current.right;
+    int val;
+    int count = 0;
+    void helper(TreeNode root, int k){
+        if(root == null){
+            return;
         }
-        
-        // If k is out of bounds, return -1 or throw an exception
-        throw new IllegalArgumentException("k is out of the bounds of the tree");
+        helper(root.left, k);
+        count++;
+        if(count == k){
+            val = root.val;
+        }
+        helper(root.right, k);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root, k);
+        return val;
+
     }
 }
