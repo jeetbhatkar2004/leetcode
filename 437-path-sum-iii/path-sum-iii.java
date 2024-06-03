3551e193674214103f20236639712cdc1afeb4ce
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-03 16:43:32 -0400

    Time: 26 ms (8.81%) | Memory: 44.6 MB (11.53%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -14,37 +14,31 @@
  * }
  */
 class Solution {
-    int count = 0;
+    int count;
     List<TreeNode> list = new ArrayList<>();
-    void helper(TreeNode root, int target, Long curr){
-        // 3 is here:
+    public void helper(TreeNode root, int target, Long curr){
         if(root == null){
             return;
         }
-        //3'S TEMP = 5
-        Long temp = curr;
         curr = curr + root.val;
-        //3'S CURR = 8
         if(curr == target){
             count++;
         }
-        helper(root.left, target, curr); //WE ARE HERE FROM 5 BACK FROM 3 -> 
+        helper(root.left, target, curr);
         helper(root.right, target, curr);
-        //3's CURR = 5
-        curr = temp;
-
     }
-    void dfs(TreeNode root){
+   public void dfs(TreeNode root){
         if(root == null){
             return;
         }
         list.add(root);
         dfs(root.left);
         dfs(root.right);
-    }
+
+}
     public int pathSum(TreeNode root, int targetSum) {
         dfs(root);
-        for(int i = 0; i < list.size(); i++){
+        for(int i = 0; i<list.size() ; i++){
             helper(list.get(i), targetSum, 0l);
         }
         return count;
