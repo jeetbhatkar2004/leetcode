2f7c4834a565572508f4bcf0d29707125e85a756
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-04 14:24:58 -0400

    Time: 1 ms (85.87%) | Memory: 42.1 MB (5.18%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 0;
+        int end = x;
+        int mid = 0;
+        Long curr = 0l;
+        while(start <= end){
+            mid = start + (end - start)/2;
+            curr = (long) mid * mid;
+            if(curr == x){
+                return mid;
+            }
+            if (curr > x){
+                end = mid -1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        if(curr > x){
+            return mid - 1;
+        }
+        else{
+            return mid;
+        }
+        
+    }
+}
