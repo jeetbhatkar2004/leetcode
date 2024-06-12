75da8a5d75155c4c274e29cfd34d340750c25ef1
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-12 16:06:24 -0400

    Time: 8 ms (21.72%) | Memory: 45.6 MB (21.25%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,47 @@
+class Solution {
+    Map<Integer, List<Integer>> hashmap = new HashMap<>();
+    Set<Integer> set = new HashSet<>();
+    Set<Integer> visited = new HashSet<>();
+    boolean valid = true;
+    void helper(int i){
+        if(!hashmap.containsKey(i)){
+            return;
+        }
+        if(set.contains(i)){
+            valid = false;
+            return;
+        }
+        if(visited.contains(i)){
+            return;
+        }
+        set.add(i);
+        for(int j = 0; j < hashmap.get(i).size(); j++){
+            if(visited.contains(hashmap.get(i).get(j))){
+                continue;
+            }
+            helper(hashmap.get(i).get(j));
+        }
+        set.remove(i);
+        visited.add(i);
+
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int i = 0;
+        while(i < prerequisites.length){
+            List<Integer> prereq = new ArrayList<>();
+            if(hashmap.containsKey(prerequisites[i][0])){
+                hashmap.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            }
+            else{
+                prereq.add(prerequisites[i][1]);
+                hashmap.put(prerequisites[i][0], prereq);
+            }
+            i++;
+        }
+        for(Map.Entry<Integer, List<Integer>> curr : hashmap.entrySet()){
+            set.clear();
+            helper(curr.getKey());
+        }
+        return valid;
+    }
+}
