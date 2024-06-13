992dc2828b3733c5236c5369b2daeeeba629f603
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-13 12:43:31 -0400

    Time: 1 ms (98.99%) | Memory: 45 MB (89.37%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,43 +1,43 @@
 class Solution {
-    void helper(char[][] grid, int i , int j){
+    void helper(char[][] grid, int i, int j){
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 'O'){
             return;
         }
         grid[i][j] = '2';
-        helper(grid, i + 1, j);
-        helper(grid, i - 1, j);
+        helper(grid, i+1, j);
+        helper(grid, i-1, j);
         helper(grid, i, j+1);
         helper(grid, i, j-1);
 
     }
     public void solve(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length; 
+        int n = grid.length;
+        int m = grid[0].length;
         int i = 0;
         int j = 0;
-        while(i < m){
-            if(grid[i][n-1] == 'O'){
-                helper(grid, i, n-1);
-            }
+        while(i< n){
             if(grid[i][0] == 'O'){
                 helper(grid, i, 0);
             }
+            if(grid[i][m-1] == 'O'){
+                helper(grid, i, m-1);
+            }
             i++;
         }
-        while(j < n){
-            if(grid[m-1][j] == 'O'){
-                helper(grid, m-1, j);
-            }
+        while(j < m){
             if(grid[0][j] == 'O'){
                 helper(grid, 0, j);
+            }
+            if(grid[n-1][j] == 'O'){
+                helper(grid, n-1, j);
             }
             j++;
         }
         i = 0;
         j = 0;
-        while(i < m){
+        while(i < n){
             j = 0;
-            while(j < n){
+            while(j < m){
                 if(grid[i][j] != '2'){
                     grid[i][j] = 'X';
                 }
@@ -48,5 +48,6 @@ class Solution {
             }
             i++;
         }
+
     }
 }
