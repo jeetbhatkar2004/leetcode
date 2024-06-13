a615ef87a5e7fef1a8e3ffcb6500986de53608d8
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-13 19:06:26 -0400

    Time: 4 ms (40.54%) | Memory: 44.1 MB (32.50%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,40 @@
+import java.util.Map;
+import java.util.Stack;
+import java.util.AbstractMap;
+
+class Solution {
+    class Pair{
+        int val;
+        int index;
+        Pair(int val, int index){
+            this.val = val;
+            this.index = index;
+        }
+    }
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        Arrays.fill(result, -1);
+        int i = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        while(i < nums1.length){
+            map.put(nums1[i], i);
+            i++;
+        }
+        i = 0;
+        Stack<Pair> stack = new Stack<>();
+        while(i < nums2.length){
+            while(!stack.isEmpty() && stack.peek().val < nums2[i]){
+                Pair temp = stack.pop();
+                if(map.containsKey(temp.val)){
+                    result[map.get(temp.val)] = nums2[i];
+                }
+            }
+            Pair curr = new Pair(nums2[i], i);
+            stack.push(curr);
+            i++;
+
+        }
+        return result;
+        
+    }
+}
