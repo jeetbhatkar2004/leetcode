7bc070fe2236c04e351dc51a716c283f4c4c1c00
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-17 21:22:59 -0400

    Time: 7 ms (33.32%) | Memory: 45 MB (44.77%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,45 +1,46 @@
 class Solution {
-    Set<Integer> hashset = new HashSet<>();
-    Map<Integer, List<Integer>> map = new HashMap<>();
-    Set<Integer> visited = new HashSet<>();
-    boolean valid = true;
-    void helper(int i){
-        if(!map.containsKey(i)){
-            return;
-        }
-        if(hashset.contains(i)){
-            valid = false;
-            return;
-        }
-        if(visited.contains(i)){
-            return;
-        }
-        hashset.add(i);
-        List<Integer> temp = map.get(i);
-        for(int j = 0; j < temp.size(); j++){
-            helper(temp.get(j));
-        }
-        hashset.remove(i);
-        visited.add(i);
-    }
+    // O(V + E)
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int i = 0;
-        while(i < prerequisites.length){
-            if(map.containsKey(prerequisites[i][0])){
-                map.get(prerequisites[i][0]).add(prerequisites[i][1]);
-            }
-            else{
-                List<Integer> temp = new ArrayList<>();
-                temp.add(prerequisites[i][1]);
-                map.put(prerequisites[i][0], temp);
-            }
-            i++;
-        }
-        for(Map.Entry<Integer, List<Integer>> curr : map.entrySet()){
-            helper(curr.getKey());
-        }
-        return valid;
+        // build a graph
+        var graph = new HashMap<Integer, ArrayList<Integer>>();
+        for (int[] prerequisite : prerequisites) {
+            var from = prerequisite[0];
+            var to = prerequisite[1];
 
+            if (!graph.containsKey(from)) graph.put(from, new ArrayList<>());
+            if (!graph.containsKey(to)) graph.put(to, new ArrayList<>());
 
+            graph.get(from).add(to);
+        }
+
+        // DFS
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : graph.entrySet()) {
+            var node = entry.getKey();
+            var visited = new HashSet<Integer>();
+            if (!DFS(graph, node, visited)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Boolean DFS(HashMap<Integer, ArrayList<Integer>> graph, Integer curr, Set<Integer> visited) {
+        if (visited.contains(curr)) return false; // Detected a loop! return false
+
+        if (graph.get(curr).isEmpty()) return true; // Reached the end
+
+        visited.add(curr);
+
+        for (var neighbor : graph.get(curr)) {
+            if (!DFS(graph, neighbor, visited)) return false;
+        }
+
+        // we have completed traversing this path.
+        // Remove it from the set
+        // also set the neighbors to empty so that it reaches line 29 and returns true immediately
+        visited.remove(curr);
+        graph.put(curr, new ArrayList<>());
+
+        return true;
     }
 }
