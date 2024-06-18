a1289a85d64675d03a007e42ddd681f84504ce40
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-18 19:53:55 -0400

    Time: 1 ms (93.52%) | Memory: 44.7 MB (14.97%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,34 +1,32 @@
 class Solution {
-    public void swap(int[] nums, int i, int j){
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(result, nums, 0);
+        return result;
+        
+    }
+    private void helper(List<List<Integer>> result, int[] nums, int start){
+        if (start == nums.length){
+            result.add(toList(nums));
+
+        }
+            for(int i = start; i < nums.length; i++){
+                swap(i, start, nums);
+                helper(result, nums, start+1);
+                swap(i, start, nums);
+            }
+        
+    }
+    private void swap(int i, int j, int[] nums){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    public void helper(int[] nums, int start, List<List<Integer>> result){
-        if(start == nums.length-1){
-            result.add(arrayToList(nums));
-            return;
+    private List<Integer> toList(int[] nums){
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            temp.add(nums[i]);
         }
-        for(int i = start; i < nums.length;i++){
-            swap(nums, start, i);
-            helper(nums, start+1, result);
-            swap(nums, start, i);
-        }
-    }
-    public List<Integer> arrayToList(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for (int num : nums) {
-            list.add(num);
-        }
-        return list;
-    }
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(nums.length < 1){
-            return result;
-        }
-        helper(nums, 0, result);
-        return result;
-        
+        return temp;
     }
 }
