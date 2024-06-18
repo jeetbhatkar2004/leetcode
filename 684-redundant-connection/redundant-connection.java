f3b2d670f78070d0ed069d5373e25783a39ba319
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-17 21:49:13 -0400

    Time: 214 ms (5.34%) | Memory: 45.3 MB (5.38%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    private Map<Integer, List<Integer>> graph = new HashMap<>();
+
+    private boolean dfs(int node, int parent, Set<Integer> visited) {
+        visited.add(node);
+        if (graph.containsKey(node)) {
+            for (int neighbor : graph.get(node)) {
+                if (neighbor == parent) {
+                    continue; 
+                }
+                if (visited.contains(neighbor) || dfs(neighbor, node, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int[] result = new int[2];
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            graph.putIfAbsent(u, new ArrayList<>());
+            graph.putIfAbsent(v, new ArrayList<>());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+
+            Set<Integer> visited = new HashSet<>();
+            if (dfs(u, -1, visited)) {
+                result = edge;
+                break;
+            }
+        }
+        return result;
+    }
+}
