770cdf091aaf8487cd974d96bb4b8341054e3891
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-06-30 20:51:39 -0400

    Time: 5 ms (84.11%) | Memory: 45.8 MB (35.18%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -1,11 +1,12 @@
 class Solution {
     int res = 0;
     boolean isExit(char[][] maze, int[] curr, int[] entrance){
-    if ((curr[0] == 0 || curr[0] == maze.length - 1 || curr[1] == 0 || curr[1] == maze[0].length - 1) && !(curr[0] == entrance[0] && curr[1] == entrance[1])) {
+        if((curr[0] == 0 || curr[1] == 0 || curr[0] == maze.length - 1 || curr[1] == maze[0].length - 1) && !(Arrays.equals(curr, entrance))){
             return true;
         }
         return false;
-    }        
+    }
+
 
     public int nearestExit(char[][] maze, int[] entrance) {
         Queue<int[]> qu = new LinkedList<>();
