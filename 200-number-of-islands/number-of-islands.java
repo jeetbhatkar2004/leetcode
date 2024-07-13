34a859155ecc1a86c3c2ff2a23b198e8e0e32182
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-07-13 22:46:40 +0530

    Time: 3 ms (84.96%) | Memory: 49.3 MB (64.37%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,31 +3,22 @@ class Solution {
         if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1'){
             return;
         }
-        grid[i][j] = '2';
+        grid[i][j] = 'x';
         helper(grid, i + 1, j);
         helper(grid, i - 1, j);
         helper(grid, i, j + 1);
         helper(grid, i, j - 1);
-
     }
     public int numIslands(char[][] grid) {
-        int i = 0;
-        int j = 0;
         int result = 0;
-        int m = grid.length;
-        int n = grid[0].length;
-        while(i < m){
-            j = 0;
-            while(j < n){
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
-                    helper(grid, i , j);
+                    helper(grid, i, j);
                     result++;
                 }
-                j++;
             }
-            i++;
         }
         return result;
-
     }
 }
