1963554b8bdbad9d4940c4619be10b69268aa756
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-07-14 11:53:33 +0530

    Time: 209 ms (11.08%) | Memory: 45.1 MB (96.22%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        boolean[] dp = new boolean[n];
-        dp[0] = true;  // Start position is always reachable
-        
-        for (int i = 0; i < n; i++) {
-            if (dp[i]) {  // If position i is reachable
-                int maxJump = Math.min(i + nums[i], n - 1);  // Ensure we don't go out of bounds
-                for (int j = i + 1; j <= maxJump; j++) {
-                    dp[j] = true;  // Mark all reachable positions from i as reachable
+        boolean[] arr = new boolean[nums.length];
+        arr[0] = true;
+        int i = 0;
+        while(i < nums.length){
+            if(arr[i]){
+                int max = Math.min(i + nums[i], nums.length - 1);
+                for(int j = i + 1; j <= max; j++){
+                    arr[j] = true;
                 }
             }
+            i++;
         }
-        
-        return dp[n - 1];  // Return whether the last position is reachable
-    }
-}
+        return arr[nums.length - 1];
+}}
