7af208f77bbe1d23c9ccb39b21b5e3eaab580b71
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-07-21 19:08:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (49.25%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,34 +1,34 @@
 class Solution {
     List<String> result = new ArrayList<>();
-    
-    void helper(String digits, Map<Character, String> phone, int start, StringBuilder curr){
-        if(curr.length() == digits.length()){
+    void helper(String digits, Map<Character, char[]> map, StringBuilder curr, int start){
+        if (start == digits.length()){
             result.add(curr.toString());
             return;
+
         }
-        String temp = phone.get(digits.charAt(start));
-        for(int i = 0; i < temp.length(); i++){
-            curr.append(temp.charAt(i));
-            helper(digits, phone, start + 1, curr);
+        char[] temp = map.get(digits.charAt(start));
+        for(int i = 0; i < temp.length; i++){
+            curr.append(temp[i]);
+            helper(digits, map, curr, start + 1);
             curr.deleteCharAt(curr.length() - 1);
         }
+
     }
     public List<String> letterCombinations(String digits) {
-        Map<Character, String> phone = new HashMap<>();
+        Map<Character, char[]> map = new HashMap<>();
+        map.put('2', new char[] {'a','b','c'});
+        map.put('3', new char[] {'d','e','f'});
+        map.put('4', new char[] {'g','h','i'});
+        map.put('5', new char[] {'j','k','l'});
+        map.put('6', new char[] {'m','n','o'});
+        map.put('7', new char[] {'p','q','r','s'});
+        map.put('8', new char[] {'t','u','v'});
+        map.put('9', new char[] {'w','x','y','z'});
+        List<String> r = new ArrayList<>();
         if(digits.length() == 0){
-            return result;
+            return r;
         }
-        phone.put('2', "abc");
-        phone.put('3', "def");
-        phone.put('4', "ghi");
-        phone.put('5', "jkl");
-        phone.put('6', "mno");
-        phone.put('7', "pqrs");
-        phone.put('8', "tuv");
-        phone.put('9', "wxyz");
-
-        StringBuilder curr = new StringBuilder();
-        helper(digits, phone, 0, curr);
+        helper(digits, map, new StringBuilder(), 0);
         return result;
     }
 }
