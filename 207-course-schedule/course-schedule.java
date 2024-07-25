ff1af7ed23d579437ba9f9afdce8ebc5ad464d95
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-07-25 11:42:45 +0530

    Time: 8 ms (19.37%) | Memory: 46.2 MB (5.19%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,44 +1,45 @@
 class Solution {
-    Set<Integer> set = new HashSet<>();
-    Set<Integer> cycle = new HashSet<>();
+    Map<Integer, List<Integer>> map = new HashMap<>();
     boolean valid = false;
-    void helper(int curr, Map<Integer, List<Integer>> map){
-        if(!map.containsKey(curr)){
+    Set<Integer> cycle = new HashSet<>();
+    Set<Integer> vis = new HashSet<>();
+
+    void helper(int start){
+        if(!map.containsKey(start)){
             return;
         }
-        if(set.contains(curr)){
-            return;
-        }
-        if(cycle.contains(curr)){
+        if(cycle.contains(start)){
             valid = true;
             return;
         }
-        cycle.add(curr);
-        List<Integer> temp = map.get(curr);
-        for(int i = 0; i < temp.size(); i++){
-            helper(temp.get(i), map);
+        if(vis.contains(start)){
+            return;
         }
-        cycle.remove(curr);
-        set.add(curr);
+        cycle.add(start);
+        for(int i = 0; i < map.get(start).size(); i++){
+            helper(map.get(start).get(i));
+        }
+        vis.add(start);
+        cycle.remove(start);
     }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
         for(int i = 0; i < prerequisites.length; i++){
             if(map.containsKey(prerequisites[i][0])){
                 map.get(prerequisites[i][0]).add(prerequisites[i][1]);
             }
             else{
-                List<Integer> curr = new ArrayList<>();
-                curr.add(prerequisites[i][1]);
-                map.put(prerequisites[i][0], curr);
+                List<Integer> list = new ArrayList<>();
+                list.add(prerequisites[i][1]);
+                map.put(prerequisites[i][0], list);
             }
         }
         for(Map.Entry<Integer, List<Integer>> curr : map.entrySet()){
-            helper(curr.getKey(), map);
+            helper(curr.getKey());
             if(valid){
                 return false;
             }
-        }   
+        }
         return true;
+
     }
 }
