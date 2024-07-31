5727c28488c401274a6d2c8bbba5d5dbccf5791e
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-07-31 16:48:41 +0530

    Time: 40 ms (6.49%) | Memory: 45 MB (33.49%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,51 +1,48 @@
-import java.util.*;
-
 class Solution {
-    Set<Integer> even = new HashSet<>();
-    Set<Integer> odd = new HashSet<>();
     Set<Integer> visited = new HashSet<>();
-    
-    public boolean isBipartite(int[][] graph) {
-        for (int start = 0; start < graph.length; start++) {
-            if (!visited.contains(start)) {
-                if (bfsCheck(graph, start) == false) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 
-    private boolean bfsCheck(int[][] graph, int start) {
+    boolean helper(int[][] graph, int j){
         Queue<Integer> qu = new LinkedList<>();
-        qu.add(start);
-        even.add(start);
-        int count = 0;
-
-        while (!qu.isEmpty()) {
+        Set<Integer> even = new HashSet<>();
+        Set<Integer> odd = new HashSet<>();
+        boolean evenbool = true;
+        qu.add(j);
+        while(!qu.isEmpty()){
             int size = qu.size();
-            while (size > 0) {
+            while(size > 0){
                 int curr = qu.poll();
-                if (count % 2 == 0) {
+                if(evenbool){
                     even.add(curr);
-                } else {
+                }
+                else{
                     odd.add(curr);
                 }
-                for (int j : graph[curr]) {
-                    if (count % 2 == 0 && even.contains(j)) {
+                visited.add(curr);
+                for(int i = 0; i < graph[curr].length; i++){
+                    if(evenbool && even.contains(graph[curr][i])){
                         return false;
                     }
-                    if (count % 2 != 0 && odd.contains(j)) {
+                    if(!evenbool && odd.contains(graph[curr][i])){
                         return false;
                     }
-                    if (!visited.contains(j)) {
-                        qu.add(j);
-                        visited.add(j);
+                    if(!visited.contains(graph[curr][i])){
+                        qu.add(graph[curr][i]);
                     }
                 }
                 size--;
             }
-            count++;
+            evenbool = !evenbool;
+        }
+        return true;
+    }
+    public boolean isBipartite(int[][] graph) {
+        for(int i = 0; i < graph.length; i++){
+            if(!visited.contains(i)){
+                boolean temp = helper(graph, i);
+                if(!temp){
+                    return false;
+                }
+            }
         }
         return true;
     }
