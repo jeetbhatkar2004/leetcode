9fcdcfff94a9d98db6da412fc6cc0767d0c98ccd
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-08-04 15:09:48 +0530

    Time: 2 ms (70.15%) | Memory: 41.7 MB (25.50%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,13 +1,11 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-        int res = 0;
         for(int i = 0; i < citations.length; i++){
             if(citations[i] >= citations.length - i){
-                res++;
+                return citations.length - i;
             }
         }
-        return res;
-
+        return 0;
     }
 }
