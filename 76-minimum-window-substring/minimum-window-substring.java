45953a281d2cb88056e61bbd13829e7cc42bf7dd
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-08-16 07:53:24 -0400

    Time: 475 ms (5.02%) | Memory: 46.3 MB (5.89%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,62 +1,65 @@
 class Solution {
-    boolean helper(Map<Character, Integer> map_s, Map<Character, Integer> map_t) {
-        for (Map.Entry<Character, Integer> curr : map_t.entrySet()) {
-            if (!map_s.containsKey(curr.getKey()) || curr.getValue() > map_s.get(curr.getKey())) {
+    boolean helper(Map<Character, Integer> map_s, Map<Character, Integer> map_t){
+        for(Map.Entry<Character, Integer> curr : map_t.entrySet()){
+            if(!map_s.containsKey(curr.getKey()) || curr.getValue() > map_s.get(curr.getKey())){
                 return false;
             }
         }
         return true;
     }
-
     public String minWindow(String s, String t) {
-        if (t.length() > s.length()) {
+        if(t.length() > s.length()){
             return "";
         }
-
+        StringBuilder curr = new StringBuilder();
         String result = "";
         int min = Integer.MAX_VALUE;
         int i = 0;
         int j = 0;
         Map<Character, Integer> map_t = new HashMap<>();
         Map<Character, Integer> map_s = new HashMap<>();
-
-        // Populate map_t with frequencies of characters in t
-        for (int k = 0; k < t.length(); k++) {
-            map_t.put(t.charAt(k), map_t.getOrDefault(t.charAt(k), 0) + 1);
-        }
-
-        while (j < s.length()) {
-            char endChar = s.charAt(j);
-            if (map_t.containsKey(endChar)) {
-                map_s.put(endChar, map_s.getOrDefault(endChar, 0) + 1);
+        for(int k = 0; k < t.length(); k++){
+            if(map_t.containsKey(t.charAt(k))){
+                int temp = map_t.get(t.charAt(k));
+                temp++;
+                map_t.put(t.charAt(k), temp);
             }
-
-            // Try to update the result and shrink the window
-            if (helper(map_s, map_t)) {
-                // Move `i` to the right as much as possible while the window is valid
-                while (helper(map_s, map_t)) {
-                    // Update the result if the current window is smaller
-                    if (min > j - i + 1) {
-                        min = j - i + 1;
-                        result = s.substring(i, j + 1);
-                    }
-
-                    // Shrink the window from the left
-                    char startChar = s.charAt(i);
-                    if (map_s.containsKey(startChar)) {
-                        map_s.put(startChar, map_s.get(startChar) - 1);
-                        if (map_s.get(startChar) == 0) {
-                            map_s.remove(startChar);
-                        }
-                    }
-
-                    i++;
+            else{
+                map_t.put(t.charAt(k), 1);
+            }
+        }
+        while(j < s.length()){
+            curr.append(s.charAt(j));
+            if(map_t.containsKey(s.charAt(j))){
+                if(map_s.containsKey(s.charAt(j))){
+                    int temp = map_s.get(s.charAt(j));
+                    temp++;
+                    map_s.put(s.charAt(j), temp);
+                }
+                else{
+                    map_s.put(s.charAt(j), 1);
                 }
             }
-
+            while(helper(map_s, map_t)){
+                if(min > j - i + 1){
+                    min = j - i + 1;
+                    result = curr.toString();
+                }
+                if(map_s.containsKey(s.charAt(i))){
+                    int temp = map_s.get(s.charAt(i));
+                    temp--;
+                    if(temp == 0){
+                        map_s.remove(s.charAt(i));
+                    }
+                    else{
+                        map_s.put(s.charAt(i), temp);
+                    }
+                }
+                curr.deleteCharAt(0);
+                i++;
+            }
             j++;
         }
-
         return result;
     }
 }
