ff70fad5aed7384c1f53ed9712a97042680a1982
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-08-21 10:33:33 -0400

    Time: 19 ms (62.75%) | Memory: 44.7 MB (88.56%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -18,16 +18,15 @@ class Solution {
         }
         int start = 1;
         int end = max;
-        int mid = 0;
         while (start < end) {
-            mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2;
             double temp = findtime(piles, mid);
             if (temp <= h) {
-                end = mid; // Try a smaller speed
+                end = mid; 
             } else {
-                start = mid + 1; // Try a larger speed
+                start = mid + 1; 
             }
         }
-        return start; // `start` will be the minimum spee
+        return start; 
     }
 }
