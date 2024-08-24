ad10971b88912a526afe06312a6566e335a40e79
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-08-24 16:26:59 -0400

    Time: 4 ms (13.41%) | Memory: 134.4 MB (6.64%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -4,17 +4,18 @@ class Solution {
         int min1 = Integer.MAX_VALUE;
         int min2 = Integer.MAX_VALUE;
         while(i < nums.length){
-            if(nums[i] <= min1){
+            if(min1 > nums[i]){
                 min1 = nums[i];
             }
-            else if(nums[i] <= min2){
+            else if (min2 > nums[i] && nums[i] != min1){
                 min2 = nums[i];
             }
-            else{
+            if(nums[i] > min1 && nums[i] > min2){
                 return true;
             }
             i++;
         }
         return false;
+
     }
 }
