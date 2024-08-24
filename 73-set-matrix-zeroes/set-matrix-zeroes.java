8f72d536279f2de9e4fc43c362b7b38fb2c626d0
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-08-24 16:50:50 -0400

    Time: 1 ms (79.48%) | Memory: 45.7 MB (29.52%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,56 +1,34 @@
 class Solution {
-    void helper(int[][] matrix, int i, int j){     
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int tempi = i;
-        int tempj = j;
-        i = 0;
-        j = 0;
-        while(i < m){
-            if(matrix[i][tempj] == 0){
+    void helper(int[][] nums, int xcoord, int ycoord){
+        for(int i = 0; i < nums[0].length; i++){
+            if(nums[xcoord][i] == 0){
+                continue;
             }
-            else{
-                matrix[i][tempj] = 4532;
-            }
-            i++;
+            nums[xcoord][i] = 758445;
         }
-        while(j < n){
-            if(matrix[tempi][j] == 0){
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i][ycoord] == 0){
+                continue;
             }
-            else{
-                matrix[tempi][j] = 4532;
-            }
-            j++;
+            nums[i][ycoord] = 758445;
         }
-
-
     }
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int i = 0;
-        int j = 0;
-        while(i < m){
-            j = 0;
-            while(j < n){
-                if (matrix[i][j] == 0){
-                    helper(matrix, i, j);
+    public void setZeroes(int[][] nums) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums[0].length; j++){
+                if(nums[i][j] == 0){
+                    helper(nums, i, j);
                 }
-                j++;
             }
-            i++;
         }
-        i = 0;
-        j = 0;
-        while(i < m){
-            j = 0;
-            while(j < n){
-                if(matrix[i][j] == 4532){
-                    matrix[i][j] = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums[0].length; j++){
+                if(nums[i][j] == 758445){
+                    nums[i][j] = 0;
                 }
-                j++;
             }
-            i++;
         }
+        
     }
 }
