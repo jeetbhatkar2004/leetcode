ecea9c45d6098eac2a9f6861524cd61338666c97
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-10-09 15:15:53 -0400

    Time: 0 ms (100.00%) | Memory: 44.3 MB (82.46%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,32 +1,33 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        helper(result, nums, 0);
-        return result;
-        
-    }
-    private void helper(List<List<Integer>> result, int[] nums, int start){
+    List<List<Integer>> result = new ArrayList<>();
+    void helper(int[] nums, int start){
         if (start == nums.length){
-            result.add(toList(nums));
-
+            result.add(toArray(nums));
+            return;
         }
-            for(int i = start; i < nums.length; i++){
-                swap(i, start, nums);
-                helper(result, nums, start+1);
-                swap(i, start, nums);
-            }
-        
+        for(int i = start; i < nums.length; i++){
+            swap(nums,i, start);
+            helper(nums, start + 1);
+            swap(nums, i, start);
+        }
     }
-    private void swap(int i, int j, int[] nums){
+    List<Integer> toArray(int[] nums){
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            res.add(nums[i]);
+        }
+        return res;
+    }
+    void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    private List<Integer> toList(int[] nums){
-        List<Integer> temp = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++){
-            temp.add(nums[i]);
-        }
-        return temp;
+    public List<List<Integer>> permute(int[] nums) {
+        helper(nums, 0);
+        return result;
+        
     }
+
+
 }
