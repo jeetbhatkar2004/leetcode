53ae9ef4e981dc27e47fbd670f1942c1e8782901
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-10-17 10:49:10 -0400

    Time: 10 ms (17.96%) | Memory: 45.9 MB (17.94%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,26 +1,27 @@
 class Solution {
     Map<Integer, List<Integer>> map = new HashMap<>();
-    boolean valid = false;
-    Set<Integer> cycle = new HashSet<>();
-    Set<Integer> vis = new HashSet<>();
+    Set<Integer> visited = new HashSet<>();
+    Set<Integer> path = new HashSet<>();
 
-    void helper(int start){
-        if(!map.containsKey(start)){
+    boolean canfin = true;
+    void helper(int i){
+        if(!map.containsKey(i)){
+            return;
+        }   
+        if(path.contains(i)){
+            canfin = false;
             return;
         }
-        if(cycle.contains(start)){
-            valid = true;
+        if(visited.contains(i)){
             return;
         }
-        if(vis.contains(start)){
-            return;
+        path.add(i);
+        List<Integer> list = map.get(i);
+        for(int j = 0; j < list.size(); j++){
+            helper(list.get(j));
         }
-        cycle.add(start);
-        for(int i = 0; i < map.get(start).size(); i++){
-            helper(map.get(start).get(i));
-        }
-        vis.add(start);
-        cycle.remove(start);
+        path.remove(i);
+        visited.add(i);
     }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         for(int i = 0; i < prerequisites.length; i++){
@@ -28,14 +29,13 @@ class Solution {
                 map.get(prerequisites[i][0]).add(prerequisites[i][1]);
             }
             else{
-                List<Integer> list = new ArrayList<>();
-                list.add(prerequisites[i][1]);
-                map.put(prerequisites[i][0], list);
+                map.put(prerequisites[i][0], new ArrayList<>());
+                map.get(prerequisites[i][0]).add(prerequisites[i][1]);
             }
         }
         for(Map.Entry<Integer, List<Integer>> curr : map.entrySet()){
             helper(curr.getKey());
-            if(valid){
+            if(!canfin){
                 return false;
             }
         }
