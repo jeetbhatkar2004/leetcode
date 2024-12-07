fdb301b9236a05e0b19440e12a5c69632aa86eed
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-12-07 16:07:31 -0500

    Time: 130 ms (88.92%) | Memory: 41.8 MB (35.26%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,32 +1,35 @@
 class Solution {
-    boolean valid = false;
-    void helper(char[][] board, String word, int curr, int i , int j){
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(curr)){
+    boolean result = false;
+    void helper(char[][] board, String word, int i, int j, int start){
+        if(start >= word.length()){
+            result = true;
+            return;
+        }
+        if(i >= board.length || j >= board[0].length || i < 0 || j < 0 || board[i][j] != word.charAt(start)){
             return;
         }
         char temp = board[i][j];
         board[i][j] = '*';
-        curr++;
-        if(curr == word.length()){
-            valid = true;
-            return;
-        }
-        helper(board, word, curr, i + 1, j);
-        helper(board, word, curr, i - 1, j);
-        helper(board, word, curr, i, j + 1);
-        helper(board, word, curr, i, j - 1);
+        helper(board, word, i + 1, j, start + 1);
+        helper(board, word, i - 1, j, start + 1);
+        helper(board, word, i, j + 1, start + 1);
+        helper(board, word, i, j - 1, start + 1);
         board[i][j] = temp;
+
 
     }
     public boolean exist(char[][] board, String word) {
         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length ; j++){
+            for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == word.charAt(0)){
-                    helper(board, word, 0, i, j);
+                    helper(board, word, i, j, 0);
+                }
+                if(result){
+                    return true;
                 }
             }
         }
-        return valid;
-
+        return false;
+        
     }
 }
