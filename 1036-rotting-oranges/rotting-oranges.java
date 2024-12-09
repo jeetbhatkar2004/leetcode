0333f048d9da7d94ae5374a1bd4314f13deeba0a
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-12-09 15:03:19 -0500

    Time: 1 ms (100.00%) | Memory: 42.1 MB (79.81%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,65 +1,58 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        boolean foundone = false;
+        boolean oneexists = false;
         Queue<int[]> qu = new LinkedList<>();
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 2){
-                    qu.add(new int[] {i,j});
+                    qu.add(new int[] {i, j});
                 }
                 if(grid[i][j] == 1){
-                    foundone = true;
+                    oneexists = true;
                 }
             }
         }
-        if(!foundone){
+        if(!oneexists){
             return 0;
         }
-        int count = 0;
+        int level = 0;
         while(!qu.isEmpty()){
             int size = qu.size();
             while(size > 0){
                 int[] curr = qu.poll();
-                int i = curr[0];
-                int j = curr[1];
-                if(i + 1 < grid.length && grid[i+1][j] == 1){
-                    qu.add(new int[] {i + 1, j});
-                    grid[i + 1][j] = -1;
-
+                int x = curr[0];
+                int y = curr[1];
+                if(x < grid.length - 1 && grid[x + 1][y] == 1){
+                    grid[x+1][y] = 2;
+                    qu.add(new int[] {x+1, y});
                 }
-                if(j + 1 < grid[0].length && grid[i][j + 1] == 1){
-                    qu.add(new int[] {i, j + 1});
-                    grid[i][j + 1] = -1;
-
-                }            
-                if(i > 0 && grid[i - 1][j] == 1){
-                    qu.add(new int[] {i - 1, j});
-                    grid[i - 1][j] = -1;
-
-                }               
-                if(j > 0  && grid[i][j - 1] == 1){
-                    qu.add(new int[] {i, j - 1});
-                    grid[i][j - 1] = -1;
-
+                if(y < grid[0].length - 1 && grid[x][y + 1] == 1){
+                    grid[x][y + 1] = 2;
+                    qu.add(new int[] {x, y+1});
+                }                
+                if(x > 0 && grid[x - 1][y] == 1){
+                    grid[x-1][y] = 2;
+                    qu.add(new int[] {x-1, y});
+                }                
+                if(y > 0 &&  grid[x][y - 1] == 1){
+                    grid[x][y-1] = 2;
+                    qu.add(new int[] {x, y-1});
                 }
-                size--;         
+                size--;
             }
-            count++;
+            level++;
         }
-        if (!check(grid)){
+        boolean flag = false;
+        for(int x = 0; x < grid.length; x++){
+            for(int y = 0; y < grid[0].length; y++){
+                if(grid[x][y] == 1){
+                    flag = true;
+                }
+            }
+        }
+        if(flag){
             return -1;
         }
-        return count-1;
-
-    }
-    boolean check(int[][] grid){
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j< grid[0].length; j++){
-                if(grid[i][j] == 1){
-                    return false;
-                }
-            }
-        }
-        return true;
+        return level - 1;
     }
 }
