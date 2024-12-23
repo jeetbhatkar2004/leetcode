d7fbd36a51960371f7ce5250aa706760a3ed021e
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2024-12-23 03:18:50 -0500

    Time: 0 ms (100.00%) | Memory: 40.2 MB (76.96%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,19 +1,7 @@
 class Solution {
     public int fib(int n) {
-        int[] result = new int[n+1];
-        if (n == 0){
-            return 0;
-        }
-        if (n==1){
-            return 1;
-        }
-        result[0] = 0;
-        result[1] = 1;
-        int i = 2;
-        while(i <= n){
-            result[i] = result[i-1] + result[i-2];
-            i++;
-        }
-        return result[n];
+        if (n < 2) return n;
+        double phi = (1 + Math.sqrt(5)) / 2;
+        return (int) Math.round(Math.pow(phi, n) / Math.sqrt(5));
     }
 }
