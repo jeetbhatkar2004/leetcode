7ccd87645fac728443df0a796dd12014c99a0deb
jeetbhatkar2004 <108226250+jeetbhatkar2004@users.noreply.github.com>
2025-05-08 13:38:35 -0400

    Time: 20 ms (40.58%) | Memory: 18.1 MB (12.44%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        hashset = set()
+        result = 0
+        i = 0
+        j = 0
+        while i < len(s):
+            while s[i] in hashset and j < len(s):
+                hashset.remove(s[j])
+                j = j + 1
+            hashset.add(s[i])
+            result = max(result, len(hashset))
+            i = i + 1
+        return result
+            
+
+
+
